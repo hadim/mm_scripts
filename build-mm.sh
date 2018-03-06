f626afd75f15344ba0946f556c4b4739a8ad999c
Hadrien Mary <hadrien.mary@gmail.com>
2018-03-06 11:00:23 -0500

    Update build-mm.sh

--- build-mm.sh
+++ build-mm.sh
@@ -4,35 +4,40 @@ set -e
 # Script used to build MM from source (on Linux/Ubuntu)
 
 # Dependencies (ubuntu / debian)
-# sudo apt-get install subversion build-essential autoconf automake libtool pkg-config libboost1.54-all-dev zlib1g-dev swig openjdk-7-jdk ant python-dev python-numpy-dev
+# sudo apt-get install subversion build-essential autoconf automake libtool libboost-dev pkg-config zlib1g-dev swig ant python-dev python-numpy-dev
 
 
 CURR_DIR=$(pwd)
 
-if [ ! -f "ij149.zip" ]; then
-    echo "Please download ij149.zip with wget http://rsb.info.nih.gov/ij/download/zips/ij149.zip"
-    exit 1
+if [ ! -f "fiji-linux64.zip" ]; then
+    echo "Downloading Fiji."
+    wget "https://downloads.imagej.net/fiji/latest/fiji-linux64.zip"
 fi
 
-rm -fr ImageJ/
-if [ ! -d "ImageJ" ]; then
-  unzip ij*.zip
+rm -fr Fiji.app/
+if [ ! -d "Fiji.app" ]; then
+  unzip "fiji-linux64.zip"
 fi
 
-if [ ! -f "ImageJ/MMConfig_demo.cfg" ]; then
-  wget https://raw.githubusercontent.com/micro-manager/micro-manager/master/bindist/any-platform/MMConfig_demo.cfg -O ImageJ/MMConfig_demo.cfg
+if [ ! -f "Fiji.app/MMConfig_demo.cfg" ]; then
+  wget "https://raw.githubusercontent.com/micro-manager/micro-manager/master/bindist/any-platform/MMConfig_demo.cfg" -O "Fiji.app/MMConfig_demo.cfg"
 fi
 
 if [ ! -d "micro-manager" ]; then
   git clone https://github.com/micro-manager/micro-manager.git
 fi
 
+svn checkout "https://valelab4.ucsf.edu/svn/3rdpartypublic/"
+
 cd micro-manager/
 GIT_HASH=$(git rev-parse --short HEAD)
 
 # Build MM
 ./autogen.sh
-CC=gcc CXX=g++ ./configure --enable-imagej-plugin=$CURR_DIR/ImageJ JAVA_HOME=/usr/lib/jvm/default-java/
+CC=gcc CXX=g++ ./configure --enable-imagej-plugin="$CURR_DIR/Fiji.app" \
+						   --with-ij-jar="$CURR_DIR/Fiji.app/jars/ij-1.51v-SNAPSHOT.jar" \
+						   JAVA_HOME="/usr/lib/jvm/default-java/"
+
 make fetchdeps
 make
 
@@ -43,4 +48,4 @@ cd ../
 # Generate zip bundle
 mkdir -p bundles/
 BUNDLE_NAME="$(date +"%Y.%m.%d.%H.%M").MicroManager-$GIT_HASH.zip"
-zip -r bundles/$BUNDLE_NAME ImageJ/
+zip -r bundles/$BUNDLE_NAME Fiji.app/
