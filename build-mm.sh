13212ad6162856a83002c5073a78871e5bceb827
Hadrien Mary <hadrien.mary@gmail.com>
2018-03-06 20:54:16 -0500

    Update script

--- build-mm.sh
+++ build-mm.sh
@@ -4,32 +4,55 @@ set -e
 # Script used to build MM from source (on Linux/Ubuntu)
 
 # Dependencies (ubuntu / debian)
-# sudo apt-get install subversion build-essential autoconf automake libtool libboost-dev \
-#                      pkg-config zlib1g-dev swig ant python-dev python-numpy-dev
+# sudo apt install build-essential autoconf automake libtool pkg-config zlib1g-dev swig ant clojure
 
 
 ROOT_DIR=$(pwd)
 IJ_DIR="$ROOT_DIR/Fiji.app"
 MM_DIR="$ROOT_DIR/micro-manager"
+THIRDPARTY_DIR="$ROOT_DIR/3rdpartypublic"
+BOOST_DIR="$THIRDPARTY_DIR/boost-linux-x86_64"
+
+# Install ImageJ/Fiji
 
 if [ ! -f "fiji-linux64.zip" ]; then
     echo "Downloading Fiji."
     wget "https://downloads.imagej.net/fiji/latest/fiji-linux64.zip"
 fi
 
-#rm -fr "$IJ_DIR"
+rm -fr "$IJ_DIR"
 if [ ! -d "$IJ_DIR" ]; then
     unzip "fiji-linux64.zip"
 fi
 
-mkdir -p "3rdpartypublic/classext"
-if [ ! -f "3rdpartypublic/classext/iconloader.jar" ]; then
-    wget -P "3rdpartypublic/classext" "https://valelab4.ucsf.edu/svn/3rdpartypublic/classext/iconloader.jar"
+# Install dependencies (ideally everything would be managed by Maven...)
+
+mkdir -p "$THIRDPARTY_DIR/classext"
+if [ ! -f "$THIRDPARTY_DIR/classext/iconloader.jar" ]; then
+    wget -P "$3RDPARTY_DIR/classext" "https://valelab4.ucsf.edu/svn/3rdpartypublic/classext/iconloader.jar"
 fi
 
-if [ ! -f "3rdpartypublic/classext/TSFProto.jar" ]; then
-    wget -P "3rdpartypublic/classext" "https://valelab4.ucsf.edu/svn/3rdpartypublic/classext/TSFProto.jar"
+if [ ! -f "$THIRDPARTY_DIR/classext/TSFProto.jar" ]; then
+    wget -P "$THIRDPARTY_DIR/classext" "https://valelab4.ucsf.edu/svn/3rdpartypublic/classext/TSFProto.jar"
 fi
+
+if [ ! -f "$THIRDPARTY_DIR/classext/clooj.jar" ]; then
+    wget -P "$THIRDPARTY_DIR/classext" "https://valelab4.ucsf.edu/svn/3rdpartypublic/classext/clooj.jar"
+fi
+
+# Boost shipped by Ubuntu is now to recent and does not work well with Micro-manager.
+# So we install an older version (1.57).
+
+if [ ! -d "$BOOST_DIR" ]; then
+	mkdir -p "$BOOST_DIR"
+	cd "$BOOST_DIR"
+    wget "https://anaconda.org/anaconda/boost/1.57.0/download/linux-64/boost-1.57.0-4.tar.bz2"
+    tar -jxvf "boost-1.57.0-4.tar.bz2"
+    rm "boost-1.57.0-4.tar.bz2"
+    cd ../../
+fi
+
+# Clone the git repository
 
 if [ ! -d "$MM_DIR" ]; then
   git clone https://github.com/micro-manager/micro-manager.git
@@ -38,20 +61,30 @@ fi
 cd "$MM_DIR"
 GIT_HASH=$(git rev-parse --short HEAD)
 
+# Launch the build process (it can take a while)
+
 ./autogen.sh
 ./configure --enable-imagej-plugin="$IJ_DIR" \
             --with-ij-jar="$IJ_DIR/jars/ij-1.51v-SNAPSHOT.jar" \
-            JAVA_HOME="/usr/lib/jvm/default-java/" CC="gcc" CXX="g++"
+            --with-boost="$BOOST_DIR" \
+            LDFLAGS=-L"$BOOST_DIR/lib"
 
 make fetchdeps
 make
 
+# Install Micro-Manager
+
 make install
 cp "$MM_DIR/bindist/any-platform/MMConfig_demo.cfg" "$IJ_DIR"
 
+# Copy boost libraries to ImageJ folder
+mkdir -p "$IJ_DIR/jars/linux64/"
+cp "$BOOST_DIR/lib/*" "$IJ_DIR/jars/linux64/"
+
 cd ../
 
-# Generate zip bundle
+# Generate zip bundle for distribution and backup
+
 mkdir -p "bundles/"
 BUNDLE_NAME="$(date +"%Y.%m.%d.%H.%M").MicroManager-$GIT_HASH.zip"
 zip -r "bundles/$BUNDLE_NAME" "$IJ_DIR"
